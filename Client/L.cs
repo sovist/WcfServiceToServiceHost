4ecfd6b7967117fd28ec0102a5a3d14eb5edbc5f
sovist <sovist9@ukr.net>
2015-11-08 20:31:01 +0200

    ++

---
+++ Client/L.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using NLog;
+
+namespace Client
+{
+    internal static class L
+    {
+        public static LoggerWithCallMemberName AppLog { get; private set; }
+
+        static L()
+        {
+             AppLog = (LoggerWithCallMemberName)LogManager.GetLogger("ExchangerServiceLog", typeof(LoggerWithCallMemberName));
+        }
+    }
+    /// <summary>
+    /// Додає в лог назву методу що його викликав
+    /// Не завжди вірновідображає метод що викливав лог, наприклад при foreach виводить MoveNext
+    /// Томущо відображає лише попередній фрейм стеку. 
+    /// </summary>
+    public class LoggerWithCallMemberName : NLog.Logger
+    {
+        public new void Error(string message)
+        {
+            base.Error((new StackTrace()).GetFrame(1).GetMethod().Name + ": " + message);
+        }
+        public new void Error(string message, params object[] args)
+        {
+            base.Error((new StackTrace()).GetFrame(1).GetMethod().Name + ": " + message, args);
+        }
+        public new void Info(string message)
+        {
+            base.Info((new StackTrace()).GetFrame(1).GetMethod().Name + ": " + message);
+        }
+        public new void Info(string message, params object[] args)
+        {
+            base.Info((new StackTrace()).GetFrame(1).GetMethod().Name + ": " + message, args);
+        }
+        public new void Warn(string message)
+        {
+            base.Warn((new StackTrace()).GetFrame(1).GetMethod().Name + ": " + message);
+        }
+        public new void Warn(string message, params object[] args)
+        {
+            base.Warn((new StackTrace()).GetFrame(1).GetMethod().Name + ": " + message, args);
+        }
+        public new void Fatal(string message)
+        {
+            base.Fatal((new StackTrace()).GetFrame(1).GetMethod().Name + ": " + message);
+        }
+        public new void Fatal(string message, params object[] args)
+        {
+            base.Fatal((new StackTrace()).GetFrame(1).GetMethod().Name + ": " + message, args);
+        }
+    }
+}
---
+++ ServiceToServiceHost/Logger.cs
@@ -0,0 +1,108 @@
+﻿using System.Diagnostics;
+
+namespace ServiceToServiceHost
+{
+    public interface ILogger
+    {
+        void Append(string message);
+    }
+    public static class Logger
+    {
+        public static void SetLoggerInstance(ILogger logger)
+        {
+            L.SetLoggerInstance(logger);
+        }
+    }
+
+    internal static class L
+    {
+        public static LoggerWriter Log { get; private set; }
+    
+        static L()
+        {
+            Log = new LoggerWriter(null);
+        }
+
+        public static void SetLoggerInstance(ILogger logger)
+        {
+            Log = new LoggerWriter(logger);
+        }
+    }
+
+    internal class LoggerWriter
+    {
+        private readonly ILogger _logger;
+        public LoggerWriter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Error(string message)
+        {
+            if (_logger == null) 
+                return;
+
+            message = string.Format("Error {0}: {1}", (new StackTrace()).GetFrame(1).GetMethod().Name, message);
+            _logger.Append(message);
+        }
+        public void Error(string format, params object[] args)
+        {
+            if (_logger == null)
+                return;
+
+            format = string.Format("Info {0}: {1}", (new StackTrace()).GetFrame(1).GetMethod().Name, string.Format(null, format, args));
+            _logger.Append(format);
+        }
+
+        public void Info(string message)
+        {
+            if (_logger == null)
+                return;
+
+            message = string.Format("Info {0}: {1}", (new StackTrace()).GetFrame(1).GetMethod().Name, message);
+            _logger.Append(message);
+        }
+        public void Info(string format, params object[] args)
+        {
+            if (_logger == null)
+                return;
+
+            format = string.Format("Info {0}: {1}", (new StackTrace()).GetFrame(1).GetMethod().Name, string.Format(null, format, args));
+            _logger.Append(format);
+        }
+
+        public void Warn(string message)
+        {
+            if (_logger == null)
+                return;
+
+            message = string.Format("Warn {0}: {1}", (new StackTrace()).GetFrame(1).GetMethod().Name, message);
+            _logger.Append(message);
+        }
+        public void Warn(string format, params object[] args)
+        {
+            if (_logger == null)
+                return;
+
+            format = string.Format("Info {0}: {1}", (new StackTrace()).GetFrame(1).GetMethod().Name, string.Format(null, format, args));
+            _logger.Append(format);
+        }
+
+        public void Fatal(string message)
+        {
+            if (_logger == null)
+                return;
+
+            message = string.Format("Fatal {0}: {1}", (new StackTrace()).GetFrame(1).GetMethod().Name, message);
+            _logger.Append(message);
+        }
+        public void Fatal(string format, params object[] args)
+        {
+            if (_logger == null)
+                return;
+
+            format = string.Format("Info {0}: {1}", (new StackTrace()).GetFrame(1).GetMethod().Name, string.Format(null, format, args));
+            _logger.Append(format);
+        }
+    }
+}
